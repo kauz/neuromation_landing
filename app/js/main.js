fb90819b20d285a28de9b8e6b5f7e917a0d3af89
Artyom Popov <kauz.qa@gmail.com>
2018-01-16 17:38:18 +0300

    cleanup

---
+++ app/js/main.js
@@ -0,0 +1,6 @@
+var navBar = document.querySelector('#navigation');
+var navBtn = document.querySelector('.nav-btn');
+navBtn.addEventListener('click', function () {
+    this.classList.toggle('open');
+    navBar.classList.toggle('--open');
+});
